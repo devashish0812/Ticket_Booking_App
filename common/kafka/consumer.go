e5d16f73f805119f385b20b395cf252dde8458b8
devashish0812 <2000520310021@ietlucknow.ac.in>
2025-11-26 22:53:13 +0530

    Added whole consumer logic in kafka module

--- common/kafka/consumer.go
+++ common/kafka/consumer.go
@@ -1,16 +1,29 @@
 package kafka
 
-import "github.com/segmentio/kafka-go"
+import (
+	"context"
+
+	"github.com/segmentio/kafka-go"
+)
 
 type consumer struct {
 	reader *kafka.Reader
 }
 
-func NewConsumer(cfg *Config, groupID string) *consumer {
+func NewConsumer(cfg *Config, groupID string, topic string) *consumer {
 	reader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:     cfg.Brokers,
 		GroupID:     groupID,
+		Topic:       topic,
 		StartOffset: kafka.FirstOffset,
 	})
 	return &consumer{reader: reader}
+}
+
+func (c *consumer) ReadMessage(ctx context.Context) (kafka.Message, error) {
+	return c.reader.ReadMessage(ctx)
+}
+
+func (c *consumer) Close() error {
+	return c.reader.Close()
 }
